012825839229fb83b3482568081ebc43595117b8
Dhymas Gabriel <dhymas.g@gmail.com>
2026-06-09 21:57:07 -0300

    chore(COND-1): initialize Spring Boot project structure

---
+++ src/main/java/com/dhymas/condominium/CondominiumFinanceApiApplication.java
@@ -0,0 +1,13 @@
+package com.dhymas.condominium;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CondominiumFinanceApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CondominiumFinanceApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dhymas/condominium/CondominiumFinanceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dhymas.condominium;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CondominiumFinanceApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
